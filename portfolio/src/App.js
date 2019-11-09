6e097b8c0a6485013833ffb28378282a7cd72cf5
divbyseptember <beatsbyseptember@gmail.com>
2019-11-09 14:07:02 -0800

    ......

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/navbar/index';
+
+const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import navBar from './components/nav/index'
+
+function App() {
+  return (
+    <div className="App">
+      <navBar />  
+    </div>
+  );
+}
+
+export default App;
